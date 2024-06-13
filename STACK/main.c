4e93b7fcd5e0ca8d408ffa686219dd3d79bdc855
Soph <111960629+sophiaacohon@users.noreply.github.com>
2024-06-13 16:29:57 +0800

    rsjhksdfjksfsdfka

--- STACK/main.c
+++ STACK/main.c
@@ -9,10 +9,12 @@ int main(int argc, char *argv[]) {
 	int option;
 	
 	do{
-		printf("1. CREATE STACK");
-		printf("2. INITALIZE STACK")';
-		printf("3. PUSH STACK")
-		printf("4. POP STACK");
+		printf("1. CREATE STACK\n");
+		printf("2. INITALIZE STACK\n")';
+		printf("3. PUSH STACK\n")
+		printf("4. POP STACK\n");
+		printf("5. DISPLAY STACK\n");
+		printf("6. VISUALIZE STACK\n");
 		printf("Enter option: ");
 		scanf("%d" &option);
 	}while(option != 0);
