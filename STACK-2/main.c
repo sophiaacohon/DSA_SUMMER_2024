526325f61e329a41ab676dc7cb5533c71d520bdf
Soph <111960629+sophiaacohon@users.noreply.github.com>
2024-06-18 15:09:22 +0800

    ill finish at home

--- STACK-2/main.c
+++ STACK-2/main.c
@@ -7,5 +7,8 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	printf("STACK ARRAY\n");
+	printf("STACK LINKED LIST\n");  
+	
 	return 0;
 }
