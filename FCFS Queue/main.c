425b15b0f8aa63bf4e3d0ded3018a84cd766b907
Soph <111960629+sophiaacohon@users.noreply.github.com>
2024-06-21 14:19:43 +0800

    added header files

--- FCFS Queue/main.c
+++ FCFS Queue/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include "FCFS_Queue.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
