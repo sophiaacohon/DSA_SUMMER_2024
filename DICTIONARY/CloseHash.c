e7b6ff1d3782440f2819312dcb557665c4939d58
Soph <sophiaannu.cohon@gmail.com>
2024-06-26 12:37:18 +0800

    made initial changes

--- DICTIONARY/CloseHash.c
+++ DICTIONARY/CloseHash.c
@@ -1,1 +1,5 @@
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "CloseHash.h"
--- DICTIONARY/main.c
+++ DICTIONARY/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include "OpenHash.h"
+#include "CloseHash.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
